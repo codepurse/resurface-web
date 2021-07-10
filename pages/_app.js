09c2313a17d06fa7b163dde4ca989f15cc45f2ae
codepurse <47669262+codepurse@users.noreply.github.com>
2021-07-11 00:25:24 +0800

    update

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,6 @@
 import '../styles/globals.scss'
-import 'tui-calendar/dist/tui-calendar.css';
-import 'tui-date-picker/dist/tui-date-picker.css';
-import 'tui-time-picker/dist/tui-time-picker.css';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
