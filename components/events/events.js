ae9b6e2e859c846f18f57a649b23539a0aee7993
codepurse <47669262+codepurse@users.noreply.github.com>
2021-07-11 14:58:16 +0800

    update

--- components/events/events.js
+++ components/events/events.js
@@ -1,19 +1,108 @@
 import React, { useState, useEffect } from "react";
 import { Calendar, momentLocalizer } from "react-big-calendar";
+import { Container, Row, Col } from "react-bootstrap";
 import moment from "moment";
 import event from "../../services/events";
-export default function App() {
-  const localizer = momentLocalizer(moment);
+import toolbar from "react-big-calendar";
 
+export default function App() {
+  const date = new Date();
+  const month = date.toLocaleString("default", { month: "long" });
+  const [selectedView , setSelectedview] = useState("month");
+
+  function customToolbar(toolbar) {
+    const goToBack = () => {
+      toolbar.onNavigate("PREV");
+    };
+
+    const goToNext = () => {
+      toolbar.onNavigate("NEXT");
+    };
+
+    const goToDay = () => {
+      toolbar.onNavigate("TODAY");
+    }
+
+    const goWeek = () => {
+      setSelectedview("week")
+    }
+
+     const goMonth = () => {
+      setSelectedview("month")
+    }
+    const goDay = () => {
+      setSelectedview("day")
+    }
+
+
+
+    return (
+      <Container fluid className="conToolbar">
+        <Row>
+          <Col lg={6}>
+            <div className="form-inline">
+              <p>
+                <img src="Image/icon/calendar_blue.png" />
+                {month} {date.getFullYear()}
+              </p>
+              <button onClick={goToBack}>
+                <img src="Image/icon/arrow_left.png" />
+              </button>
+              <button onClick={goToNext}>
+                <img src="Image/icon/arrow_right.png" />
+              </button>
+            </div>
+          </Col>
+          <Col lg={6}>
+            <div className="float-right">
+              <button className="btnNav" onClick = {goToDay}>Today</button>
+              <div
+                className="btn-group"
+                role="group"
+                aria-label="Basic example"
+              >
+                <button type="button" onClick = {goMonth} className="btn btn-secondary">
+                  Month
+                </button>
+                <button type="button" onClick = {goWeek} className="btn btn-secondary">
+                  Week
+                </button>
+                <button type="button" onClick = {goDay} className="btn btn-secondary">
+                  Day
+                </button>
+              </div>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+  const localizer = momentLocalizer(moment);
   return (
-    <div className="App h-100">
-      <Calendar
-        localizer={localizer}
-        events={event}
-        startAccessor="start"
-        endAccessor="end"
-        style={{ height: 800 }}
-      />
-    </div>
+    <>
+      <Row>
+        <Col lg={6}>
+          <p>Event Calendars</p>
+          <p>
+            In this section, you can view and manage all records with their
+            details. You can view edit many information.
+          </p>
+        </Col>
+        <Col lg={6}></Col>
+      </Row>
+      <Container fluid className="conCalendar">
+        <Calendar
+          localizer={localizer}
+          events={event}
+          view = {selectedView}
+          startAccessor="start"
+          endAccessor="end"
+          style={{ height: 800 }}
+          components={{
+            toolbar: customToolbar,
+          }}
+        />
+      </Container>
+    </>
   );
 }
