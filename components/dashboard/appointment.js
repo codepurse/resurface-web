617d74f717e4461fd0028d63b7e4003bd733bcdd
Uchihabeats <55317055+Uchihabeats@users.noreply.github.com>
2021-07-13 17:42:23 +0800

    Fetch events API

--- components/dashboard/appointment.js
+++ components/dashboard/appointment.js
@@ -5,7 +5,7 @@ import Modal from "react-bootstrap/Modal";
 import "react-datepicker/dist/react-datepicker.css";
 import Eventadd from "../dashboard/eventAdd";
 import axios from "axios";
-
+import moment from "moment";
 {
   /* Fake data */
 }
@@ -74,8 +74,6 @@ function appointment() {
                 <Table className="tableAppointment" responsive borderless>
                   <thead>
                     <tr>
-                      <th>Date</th>
-                      <th>Time</th>
                       <th>Start</th>
                       <th>End</th>
                       <th>Event Name</th>
@@ -84,39 +82,30 @@ function appointment() {
                     </tr>
                   </thead>
                   <tbody>
-                    {appointment_list.map((event) => (
+                    {event.map((event) => (
                       <tr>
                         <td>
-                          <p>{event.date}</p>
-                        </td>
-                        <td>
                           <p>
-                            <img src="../Image/icon/clock_blue.png"></img>
-                            {event.time_start} - {event.time_end}
+                            <img src="../Image/icon/duration.png"></img>
+                            {moment(event.date_from).format('MMMM Do YYYY, h:mm:ss a')}
                           </p>
                         </td>
                         <td>
                           <p>
                             <img src="../Image/icon/duration.png"></img>
-                            {event.duration}
+                            {moment(event.date_to).format('MMMM Do YYYY, h:mm:ss a')}
                           </p>
                         </td>
                         <td>
-                          <p>
-                            <img src="../Image/icon/duration.png"></img>
-                            {event.duration}
-                          </p>
-                        </td>
-                        <td>
-                          <p>{event.name}</p>
+                          <p>{event.subject}</p>
                         </td>
                         <td>
                           <p
                             className={
-                              event.type == "Business" ? "pBuss" : "pSession"
+                              event.event_type == "Business" ? "pBuss" : "pSession"
                             }
                           >
-                            {event.type}
+                            {event.event_type}
                           </p>
                         </td>
                         <td>
